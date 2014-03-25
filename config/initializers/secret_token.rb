c1d044d35ee6a39a18d150d91d3afe58aa703258
kurokocon <kuroicon@linux-1vs7.site>
2014-03-25 18:47:55 -0400

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TyphBlog::Application.config.secret_key_base = '2237da6fe51d575bbc14fd01dae1e646b4ccc43b6b3521b349905b556599248a60c5485b037949947bbcbbf655f3c1f3435d8f7cb5a9a62f94d300260485b7c0'
